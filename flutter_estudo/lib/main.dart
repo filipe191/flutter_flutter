2e40bc372d29eb0247f55eb0a819c7ac17d3407c
filipe191 <filipe.souza1000@gmail.com>
2023-03-23 18:38:05 -0300

    feat: atualizando e começando os estudo com o startto.dev

--- flutter_estudo/lib/main.dart
+++ flutter_estudo/lib/main.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      // TODO: implement buil'd
-      child: Text(
-        'Hello word',
-        textDirection: TextDirection.rtl,
-      ),
-    );
-  }
+  runApp(MaterialApp());
 }
